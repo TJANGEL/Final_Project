3fdedb6b47e9704eedc3102bb4d51981f18e05ef
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-02-28 17:02:57 -0500

    added skeleton code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/netflixtitles"
+  process.env.MONGODB_URI || "mongodb://localhost/netflixTitles"
 );
 
 // Start the API server
