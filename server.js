ccdc13d1d29b1b56c39443a740de9d3bfbfd403c
N <username@users.noreply.github.com>
2019-03-02 13:35:23 -0500

    merging

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -8,10 +7,17 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Passport authentification
+app.use(session({ secret: process.env.PASSPORT_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Add routes, both API and view
 app.use(routes);
 
