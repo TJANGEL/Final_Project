b904f0eb108ea44af9ce341bcc37068a3058d54c
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-04 13:27:41 -0500

    updated componenet

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ResultsContainer from "../components/ResultsContainer";
 import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron";
 
 class Favorites extends React.Component {
   constructor(props) {
@@ -24,6 +25,7 @@ class Favorites extends React.Component {
     console.log(this.state.favorites);
     return (
       <main>
+        <Jumbotron />
         <ResultsContainer
           favorites={this.state.favorites}
           path={this.props.match.path}
