a36a160b150d1b72027bbba30b79ac47ab6f8b1c
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-05 17:23:22 -0500

    updated navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,6 +2,25 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 
+// react-bootstrap navbar
+function Navi() {
+  //   // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">IMPATIENT NETFLIXING</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/register">Register</Nav.Link>
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/roulette">Roulette</Nav.Link>
+        <Nav.Link href="/favorites">Favorites</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default Navi;
+
 // function Navbar() {
 //   // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 //   return (
@@ -70,23 +89,3 @@ import { Navbar, Nav } from "react-bootstrap";
 // }
 
 // export default Navbar;
-
-// react-bootstrap navbar
-
-function Navi() {
-  //   // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">IMPATIENT NETFLIXING</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/register">Register</Nav.Link>
-        <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/roulette">Roulette</Nav.Link>
-        <Nav.Link href="/favorites">Favorites</Nav.Link>
-      </Nav>
-    </Navbar>
-  );
-}
-
-export default Navi;
