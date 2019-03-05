bd2ab0bdad2b3213598ec90259fb82f5e1257988
Swaroop Kondreddy <swaroopreddy.k@gmail.com>
2019-03-05 18:32:29 -0500

    roop changes to scrape

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,75 @@ import About from "./pages/About";
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
+import { inherits } from "util";
+import cheerio from 'cheerio';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mediaList: [{}]
+    }
+  }
+
+  async movieScraper(maxPageNumber) {
+    let minRating = 0, audio = 'en', minYear = 1920, order = 'date'
+    let movieArray = []
+    let maxYear = new Date().getFullYear
+    const URL = `https://flixable.com/?min-rating=${minRating}&audio=${audio}&min-year=${minYear}&max-year=${maxYear}&order=${order}&page=`
+
+    for (let start = 0; start <= maxPageNumber; start++) {
+      // console.log("scraping Page: " + start)
+      let response = await axios.get(URL + start)
+      let $ = cheerio.load(response.data);
+      $(".poster-container").each((i, element) => {
+        let imageLink = $(element).children('a').children('img').attr('src');
+        let title = $(element).children('a').children('img').attr('alt');
+        let titleLink = $(element).children('a').attr('href');
+        if (titleLink) {
+          let movieObject = {
+            title: title,
+            image: imageLink,
+            url: "https://flixable.com" + titleLink
+          }
+          movieArray.push(movieObject)
+        }
+      })
+    }
+    return movieArray
+  }
+
+  async componentWillMount() {
+    try {
+      let movieArray
+      // let response = await axios.get("https://flixable.com")
+      // let $ = cheerio.load(response.data);
+      // let pageNumbersArray = []
+      // $("li").each((i, element) => {
+      // let pageNumbers = $(element).children('a').attr('href')
+      // pageNumbersArray.push(pageNumbers)
+      // })
+
+      // let maxpageLink = pageNumbersArray[pageNumbersArray.length - 2]
+      // let index = maxpageLink.indexOf("page=")
+      // if (index > -1) {
+      //   let start = new Date().getTime();
+      //   // TODO: pass this as the parameter to the function movieScraper to scrape all the pages maxpageLink.slice(index + 5)
+      //   movieArray = await movieScraper(15)
+      //   let end = new Date().getTime();
+      //   console.log('Execution time (hr): %d ms', (end - start))
+      //   this.setState({ mediaList: movieArray })
+      //   console.log(movieArray.length)
+      // }
+      movieArray = await this.movieScraper(15)
+      this.setState({ mediaList: movieArray })
+      console.log(movieArray.length)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
