2cd708f24449a8e33f18c28e8148b6a720f93915
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-05 09:47:51 -0500

    populated Roulette Page

--- client/src/pages/Roulette.js
+++ client/src/pages/Roulette.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
+import { Row, Col, Container } from "react-bootstrap";
 
 export class Roulette extends Component {
   render() {
     return (
       <div>
-        <row>
-          <col size="md-12">
+        <Row>
+          <Col sm={12}>
             <Jumbotron>
               <h1>IMPATIENT NETFLIXING</h1>
               <h3>Roll to Decide what you'll watch next</h3>
             </Jumbotron>
-          </col>
-        </row>
+          </Col>
+        </Row>
+        <Container>
+          <Row>
+            <Col sm={8}>Parameters</Col>
+            <Col sm={4}>Result:</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
