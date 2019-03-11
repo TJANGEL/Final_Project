f2460ea5887e9b07577d8c90b6581d50a45f24ba
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-11 11:38:02 -0400

    added FlipMove to favorites page

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 import { ListGroup, Button } from "react-bootstrap";
+import Shuffle from "../components/Shuffle";
 // import { Card, CardDeck } from "react-bootstrap";
-import FlipMove from "react-flip-move";
+// import FlipMove from "react-flip-move";
 
 // import API from "../utils/API";
 
@@ -63,96 +64,9 @@ class Favorites extends React.Component {
         ) : (
           <h2>No Favorites Saved</h2>
         )} */}
-        <ListGroup className="mb-2">
-          {/* <FlipMove> */}
-          <ListGroup.Item key="a">
-            Kiss Kiss Bang Bang
-            <Button variant="outline-secondary" className="link" href="#">
-              Details
-            </Button>
-            <Button className="remove-btn" variant="outline-danger">
-              &times;
-            </Button>
-          </ListGroup.Item>
-          <ListGroup.Item key="b">
-            Avengers
-            <Button variant="outline-secondary" className="link" href="#">
-              Details
-            </Button>
-            <Button className="remove-btn" variant="outline-danger">
-              &times;
-            </Button>
-          </ListGroup.Item>
-          <ListGroup.Item key="c">
-            Wreck it Ralph
-            <Button variant="outline-secondary" className="link" href="#">
-              Details
-            </Button>
-            <Button className="remove-btn" variant="outline-danger">
-              &times;
-            </Button>
-          </ListGroup.Item>
-          <ListGroup.Item key="d">
-            Frozen
-            <Button variant="outline-secondary" className="link" href="#">
-              Details
-            </Button>
-            <Button className="remove-btn" variant="outline-danger">
-              &times;
-            </Button>
-          </ListGroup.Item>
-          {/* </FlipMove> */}
-        </ListGroup>
-        {/* <CardDeck>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Button variant="outline-secondary" className="link" href="#">
-                Details
-              </Button>
-              <Button className="remove-btn" variant="outline-danger">
-                &times;
-              </Button>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck> */}
+        <div>
+          <Shuffle />
+        </div>
       </div>
     );
   }
