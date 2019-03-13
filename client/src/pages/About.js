ec8791b6b8a0964e7cc3b897bc9a2a1420849442
Anna Stein <ann.m.stein@gmail.com>
2019-03-12 17:10:43 -0700

    about page and style updates for paragraph and image

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,6 +8,9 @@ export class About extends Component {
         <Jumbotron>
           <h1 className="jumbotron-name">ABOUT</h1>
         </Jumbotron>
+        <img alt="netflix and chill" src="..src/img/netflix-and-chill.jpg"/>
+        <p>Are you overcome with choice paralysis every time you try to Netflix and chill? If so, you like hundreds of other people will benefit from this app. Taking the guess work out of choosing a movie or show we give you a selection of genres and then suggest a movie/show for your viewing pleasure. Give the wheel a spin and see what Lady Luck has in store for you! If you love the suggested show go ahead and add it to your favorites page, you’ll just need to create an account. Want to try this out without saving your suggestions, no problem, the wheel spins for you even if you are not logged in. Go ahead, take a chance, test your luck and see what is in-store for your viewing pleasure!</p>
+        
       </div>
     );
   }
